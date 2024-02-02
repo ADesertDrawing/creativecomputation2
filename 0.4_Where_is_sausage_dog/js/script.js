844d00866f7669bb63433900529fedea56ea7030
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2024-02-02 12:49:37 +0000

    Displaying animals like a champ

--- 0.4_Where_is_sausage_dog/js/script.js
+++ 0.4_Where_is_sausage_dog/js/script.js
@@ -3,11 +3,41 @@
 by A Desert Drawing
 */
 "use strict";
-function setup() {
-    createCanvas(500, 500);
 
+const NUM_ANIMAL_IMAGES = 10;
+const NUM_ANIMALS = 100;
+
+let animalImages = [];
+let animals = [];
+
+
+function preload() {
+    for (let i = 0; i < NUM_ANIMAL_IMAGES; i++) {
+        let animalImage = loadImage(`assets/images/animal${i}.png`);
+        animalImages.push(animalImage);//animalImages is name of array
+    }
+
+}
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    for (let i = 0; i < NUM_ANIMALS; i++) {
+        let x = random(0, width);
+        let y = random(0, height);
+        let animalImage = random(animalImages);
+        //create animal with random x, y and image
+        let animal = new Animal(x, y, animalImage);
+        //add the animal that's currently inside the animal 
+        //variable to the animals array
+        animals.push(animal);
+    }
 }
 
 function draw() {
-    background(0, 0, 0);
+    background(255, 255, 0);
+    //as soon as you've created the array, you should be 
+    //using the array length so that it's dynamically calculated
+    for (let i = 0; i < animals.length; i++) {
+        //update the animal at position i
+        animals[i].update();
+    }
 }
