7a31d0670da3b5139394be37bbe8566ff5470066
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2024-02-02 10:59:16 +0000

    Started 0.3_First-class_functions

--- 0.2_Review_Object_parameters/js/script.js
+++ 0.2_Review_Object_parameters/js/script.js
@@ -8,13 +8,28 @@ function setup() {
 }
 
 function draw() {
-    drawFancyRect(250, 250, 200, 200, 255, 255, 0, CENTER);
+    let config = {
+        x: 250,
+        y: 250,
+        width: 200,
+        height: 200,
+        fillColor: {
+            r: 255,
+            g: 255,
+            b: 0
+        },
+        mode: CENTER
+    };
+    drawFancyRect(config);
 }
 
-function drawFancyRect(x, y, w, h, r, g, b, mode) {
+// NEW: We write the individual property names inside curly brackets
+// to DESTRUCTURE the object parameter into individual variables
+function drawFancyRect({ x, y, width, height, fillColor, mode }) {
+    // Then we can just use the resulting variables in the usual way
     push();
-    fill(r, g, b);
+    fill(fillColor.r, fillColor.g, fillColor.b);
     rectMode(mode);
-    rect(x, y, w, h);
+    rect(x, y, width, height);
     pop();
 }
