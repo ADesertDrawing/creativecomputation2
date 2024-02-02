5aff5eb288cc954ced7211b6a8d414c69d147487
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2024-02-02 11:17:53 +0000

    A function as an argument of another function

--- 0.3_First-class_functions/js/script.js
+++ 0.3_First-class_functions/js/script.js
@@ -3,16 +3,8 @@
 by A Desert Drawing
 */
 "use strict";
-// A function to add two numbers together and return the result
-// (Amazing I know)
-function add(a, b) {
-    return a + b;
+function hello() {
+    alert(`Hello!`); // Pop up an alert dialog that says "Hello!"
 }
 
-// We can create a new variable and ASSIGN the add function to it!
-let plus = add;
-// Now the variable "plus" has the add() function inside it
-
-// And we can then CALL the function inside plus in the usual way!
-let onePlusOne = plus(1, 1); // Call the function inside plus (which is the add() function!)
-alert(onePlusOne); // 2
+setTimeout(hello, 5000); // Call the hello() function after 5000 milliseconds
