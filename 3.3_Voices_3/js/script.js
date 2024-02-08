ffb72df53d93e6fcdf4da164ec2fa790235108dd
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2024-02-08 18:09:34 +0000

    Speech with subtitles when speaking

--- 3.3_Voices_3/js/script.js
+++ 3.3_Voices_3/js/script.js
@@ -3,11 +3,43 @@
 by A Desert Drawing
 */
 "use strict";
+
+const speechSynthesizer = new p5.Speech();
+
+let showSubtitle = false;
+let toSay = `Have you done a stinky, \nyou dirty little man?`;
+
 function setup() {
     createCanvas(500, 500);
 
+    //Set the voice settings
+    speechSynthesizer.setPitch(.9);
+    speechSynthesizer.setRate(1.2);
+    speechSynthesizer.setVoice(`Google UK English Female`);
+
+    speechSynthesizer.onStart = speechStarted;
+    speechSynthesizer.onEnd = speechEnded;
+
+    //List all the possible voices
+    console.log(speechSynthesizer.listVoices());
 }
 
 function draw() {
-    background(0, 0, 0);
+    background(165, 220, 250);
+    if (showSubtitle) {
+        textSize(30);
+        text(toSay, 100, 100);
+    }
+}
+
+function mousePressed() {
+    //Say a thing!
+    speechSynthesizer.speak(toSay);
+}
+
+function speechStarted() {
+    showSubtitle = true;
+}
+function speechEnded() {
+    showSubtitle = false;
 }
