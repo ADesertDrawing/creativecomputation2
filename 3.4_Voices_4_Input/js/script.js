265fba0b4a5f20b76cf7c7298499ddf7affb9287
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2024-02-09 12:01:19 +0000

    Basic speech recognition

--- 3.4_Voices_4_Input/js/script.js
+++ 3.4_Voices_4_Input/js/script.js
@@ -4,15 +4,23 @@ by A Desert Drawing
 */
 "use strict";
 
-
+const speechRecognizer = new p5.SpeechRec();
+let currentSpeech = `?`;
 
 function setup() {
     createCanvas(500, 500);
 
-
+    speechRecognizer.onResult = handleSpeechInput;
+    speechRecognizer.start();
 }
 
 function draw() {
-    background(30, 174, 13);
+    background(255, 100, 156);
+    textAlign(CENTER, CENTER);
+    textSize(24);
+    text(currentSpeech, width / 2, height / 2);
+}
 
+function handleSpeechInput() {
+    currentSpeech = speechRecognizer.resultString;
 }
