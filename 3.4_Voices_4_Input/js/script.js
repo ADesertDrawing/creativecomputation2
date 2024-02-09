b65e6b50fdd52a79923b26346facefe4b5a57ab8
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2024-02-09 11:50:08 +0000

    Started 3.4_Voices_4_Input

---
+++ 3.4_Voices_4_Input/js/script.js
@@ -0,0 +1,18 @@
+/**
+3.4_Voices_4_Input
+by A Desert Drawing
+*/
+"use strict";
+
+
+
+function setup() {
+    createCanvas(500, 500);
+
+
+}
+
+function draw() {
+    background(30, 174, 13);
+
+}
