f978217134c052799ee7de4babe8735f2b027919
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2024-02-09 13:34:56 +0000

    Change color depending on speech, using last word of sentence with pop

--- 3.4_Voices_4_Input/js/script.js
+++ 3.4_Voices_4_Input/js/script.js
@@ -5,31 +5,21 @@ by A Desert Drawing
 "use strict";
 
 const speechRecognizer = new p5.SpeechRec();
-let currentSpeech = `?`;
-
 
 function setup() {
     createCanvas(500, 500);
     speechRecognizer.onResult = handleSpeechInput;
     speechRecognizer.continuous = true;
+    speechRecognizer.interimResults = true;
     speechRecognizer.start();
 }
 
-
 function draw() {
-    background(0);
-    if (lightsAreOn) {
-        background(255);
-    }
+    background(backgroundColor);
 }
 
-
 function handleSpeechInput() {
-    console.log(speechRecognizer.resultString);
-    if (speechRecognizer.resultString.toLowerCase() === `turn the lights on`) {
-        lightsAreOn = true;
-    }
-    else if (speechRecognizer.resultString.toLowerCase() === `turn the lights off`) {
-        lightsAreOn = false;
-    }
+    let words = speechRecognizer.resultString.split(` `);
+
+    backgroundColor = words.pop();
 }
