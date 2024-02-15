df07129f69b0d420ad80be50975b76c951ea83d1
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2024-02-15 15:44:37 +0000

    Here's a random fortune for you

--- 3.6_JSON/js/script.js
+++ 3.6_JSON/js/script.js
@@ -4,30 +4,35 @@ by A Desert Drawing
 */
 "use strict";
 
-let tarot;
+// A global variable to store our data in
+let tarotData = undefined;
+// A global variable to store our fortune in
+let fortune = `No fortune found yet...`;
 
 function preload() {
     // Used in preload, loadJSON will just return the data into our variable
-    tarot = loadJSON("assets/data/tarot_interpretations.json");
+    tarotData = loadJSON("assets/data/tarot_interpretations.json");
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-
+    //Choose a random card
+    let card = random(tarotData.tarot_interpretations);
+    // Choose a random fortune
+    fortune = random(card.fortune_telling);
 }
 
 function draw() {
     background(0);
 
     // Get the first shadow meaning into a variable by following the path through the tarot object
-    let firstShadowMeaning = tarot.tarot_interpretations[0].meanings.shadow[0];
 
     // Display the meaning
     push();
     textSize(18);
     textAlign(CENTER);
     fill(255, 255, 0);
-    text(firstShadowMeaning, width / 2, height / 2);
+    text(fortune, width / 2, height / 2);
     pop();
 }
 
