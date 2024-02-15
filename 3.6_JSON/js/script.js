9b1ef33ae7102fbfa76f8f7a7a0081704968be80
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2024-02-15 17:40:48 +0000

    JSON from API

--- 3.6_JSON/js/script.js
+++ 3.6_JSON/js/script.js
@@ -3,36 +3,31 @@
 by A Desert Drawing
 */
 "use strict";
-
-// A global variable to store our data in
-let tarot;
-// A global variable to store our fortune in
-let fortune;
+let jokeText = ``; // The current joke.
+let jokeData = undefined; // The loaded joke data
 
 function preload() {
-    // Load the JSON data from a file online!
-    tarot = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json`);
+    jokeData = loadJSON(`https://official-joke-api.appspot.com/jokes/programming/random`);
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    //Choose a random card
-    let card = random(tarot.tarot_interpretations);
-    // Choose a random fortune
-    fortune = random(card.fortune_telling);
+
+    // We get the joke object as the first element of the array
+    let joke = jokeData[0];
+    // Set the joke text as the setup and punchline properties together
+    jokeText = `${joke.setup}\n\n${joke.punchline}`;
 }
 
 function draw() {
     background(0);
 
-    // Get the first shadow meaning into a variable by following the path through the tarot object
-
-    // Display the meaning
+    // Display the current joke
     push();
-    textSize(18);
-    textAlign(CENTER);
     fill(255, 255, 0);
-    text(fortune, width / 2, height / 2);
+    textSize(18);
+    textAlign(CENTER, CENTER);
+    text(jokeText, width / 2, height / 2);
     pop();
 }
 
