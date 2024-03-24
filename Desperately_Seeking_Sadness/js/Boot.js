b4351c28378b1e26d95171dedda0fc066f717475
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2024-03-24 18:03:17 +0000

    Finished!

--- Desperately_Seeking_Sadness/js/Boot.js
+++ Desperately_Seeking_Sadness/js/Boot.js
@@ -6,6 +6,9 @@ class Boot extends Phaser.Scene {
     }
     preload() {
         //load assets here
+        this.load.image(`avatar`, `assets/images/neutral.png`);
+        this.load.image(`thumb_down`, `assets/images/thumb_down.png`);
+        this.load.image(`thumb_up`, `assets/images/thumb_up.png`);
 
         this.load.on(`complete`, () => {
             this.scene.start(`play`);
